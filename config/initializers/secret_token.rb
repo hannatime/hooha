e0224fc9c9016e4ced828bd4b75c6016ff68c28b
Ben Hanna <ben.hanna@me.com>
2014-05-24 15:48:47 +1000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hooha::Application.config.secret_key_base = '74ef0bae3a1ab9322f70cf180a61ce639105890c3c15d86807820f0fc02ff3b493d5beb7d1f4833097aa90cb7cb0c1f062db2b2dba468e66caeb412c8a0b1bb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hooha::Application.config.session_store :cookie_store, key: '_hooha_session'
